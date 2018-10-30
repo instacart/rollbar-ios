b29a4b4909125aa0b6ea6695cc9cc28fc66f5d58
Florent Bories <florent.bories@instacart.com>
2018-10-30 10:59:05 -0700

    Fix umbrella header

--- RollbarFramework/RollbarFramework.h
+++ RollbarFramework/RollbarFramework.h
@@ -6,19 +6,6 @@ FOUNDATION_EXPORT double RollbarFrameworkVersionNumber;
 //! Project version string for RollbarFramework.
 FOUNDATION_EXPORT const unsigned char RollbarFrameworkVersionString[];
 
-<<<<<<< HEAD
-#import <RollbarFramework/Rollbar.h>
-#import <RollbarFramework/RollbarNotifier.h>
-#import <RollbarFramework/RollbarConfiguration.h>
-#import <RollbarFramework/RollbarThread.h>
-#import <RollbarFramework/RollbarFileReader.h>
-#import <RollbarFramework/RollbarReachability.h>
-#import <RollbarFramework/RollbarLogger.h>
-#import <RollbarFramework/RollbarLevel.h>
-#import <RollbarFramework/NSJSONSerialization+Rollbar.h>
-#import <RollbarFramework/RollbarTelemetry.h>
-#import <RollbarFramework/RollbarTelemetryType.h>
-=======
 #import <Rollbar/Rollbar.h>
 #import <Rollbar/RollbarNotifier.h>
 #import <Rollbar/RollbarConfiguration.h>
@@ -26,4 +13,7 @@ FOUNDATION_EXPORT const unsigned char RollbarFrameworkVersionString[];
 #import <Rollbar/RollbarFileReader.h>
 #import <Rollbar/RollbarReachability.h>
 #import <Rollbar/RollbarLogger.h>
->>>>>>> Fix umbrella header errors for Carthage support
+#import <Rollbar/RollbarLevel.h>
+#import <Rollbar/NSJSONSerialization+Rollbar.h>
+#import <Rollbar/RollbarTelemetry.h>
+#import <Rollbar/RollbarTelemetryType.h>
